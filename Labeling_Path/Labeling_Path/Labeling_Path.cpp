5ce35eaacb09c8eea361382523470039d77aa124
henockmamo54 <henockmamo54@gmail.com>
2022-03-07 18:06:13 -0800

    best path and score added

--- Labeling_Path/Labeling_Path/Labeling_Path.cpp
+++ Labeling_Path/Labeling_Path/Labeling_Path.cpp
@@ -3,37 +3,42 @@
 #include "stdafx.h"
 #include "stdio.h"
 #include "math.h"
-
+#include "string"
 #include "Labeling_Path.h"
 
 using namespace System;
 using namespace Labeling_Path;
+using namespace System::Collections::Generic;
 
 /*
 *
-*   The method computes the optimal labeling path given the 
-*   the time course of the fractional synthesis rate from 
+*   The method computes the optimal labeling path given the
+*   the time course of the fractional synthesis rate from
 *   several proteins of a peptide. The path means that the
 *   program chooses the best fractional synthesis rate time
-*   points so that the overall sequence of the fractional 
+*   points so that the overall sequence of the fractional
 *   synthesis rate is monotonically increasing.
 *   The input data is in Fractional_Synthesis_Rate
 *   It is assumed that the columns are peptides,
 *   and the rows are the time course values.
 *
 */
-int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional_Synthesis_Rate,
+int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2>^ Fractional_Synthesis_Rate,
 	int Nrow, int Ncol, int NEH, float fBWE)
 {
 	int i, j, i1, j1, i0, j0, iMax;
 
-	array <float, 2> ^Score_Fractional_Synthesis_Rate_Time = gcnew array <float, 2>(Nrow, Ncol);
+	array <float, 2>^ Score_Fractional_Synthesis_Rate_Time = gcnew array <float, 2>(Nrow, Ncol);
 
-	array <float, 2> ^fBest_Fractional_Synthesis_Rate_Time = gcnew array <float, 2>(Nrow, Ncol);
+	array <float, 2>^ fBest_Fractional_Synthesis_Rate_Time = gcnew array <float, 2>(Nrow, Ncol);
 
-	array <int, 2> ^index_X = gcnew array <int, 2>(Nrow, Ncol);
+	array <int, 2>^ index_X = gcnew array <int, 2>(Nrow, Ncol);
 
-	array <int, 2> ^index_Y = gcnew array <int, 2>(Nrow, Ncol);
+	array <int, 2>^ index_Y = gcnew array <int, 2>(Nrow, Ncol);
+
+	List < String^ >^ bestpaths = gcnew List<String^>();
+
+	List<float>^ bestpaths_score = gcnew List<float>();
 
 	float fSum, ftemp, fMinSlope;
 
@@ -53,7 +58,7 @@ int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional
 	}
 
 	for (i = 0; i < Nrow; i++)
-	//for (i = 0; i < 1; i++)
+		//for (i = 0; i < 1; i++)
 	{
 		for (j = 1; j < Ncol; j++)
 		{
@@ -64,11 +69,11 @@ int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional
 			//
 			//if (fBest_Fractional_Synthesis_Rate_Time[i, j - 1] == Fractional_Synthesis_Rate[i, j - 1] &&
 				//Fractional_Synthesis_Rate[i, j] > Fractional_Synthesis_Rate[i, j - 1] )
-			if (Fractional_Synthesis_Rate[i, j]  > fBest_Fractional_Synthesis_Rate_Time[i, j - 1])
+			if (Fractional_Synthesis_Rate[i, j] > fBest_Fractional_Synthesis_Rate_Time[i, j - 1])
 			{
 
 				Score_Fractional_Synthesis_Rate_Time[i, j] = Score_Fractional_Synthesis_Rate_Time[i, j - 1] +
-					Diff_Fraction_Rates(Fractional_Synthesis_Rate[i, j - 1], Fractional_Synthesis_Rate[i, j] );
+					Diff_Fraction_Rates(Fractional_Synthesis_Rate[i, j - 1], Fractional_Synthesis_Rate[i, j]);
 
 				index_X[i, j] = i;
 
@@ -88,7 +93,7 @@ int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional
 					fSum = Score_Fractional_Synthesis_Rate_Time[i, j - 1] +
 						//Diff_Fraction_Rates(Fractional_Synthesis_Rate[index_X[i, j - 1], index_Y[i, j - 1]], Fractional_Synthesis_Rate[i0, j]);
 
-						Diff_Fraction_Rates(fBest_Fractional_Synthesis_Rate_Time[i, j - 1], Fractional_Synthesis_Rate[i0, j] );
+						Diff_Fraction_Rates(fBest_Fractional_Synthesis_Rate_Time[i, j - 1], Fractional_Synthesis_Rate[i0, j]);
 
 					//if(7 == i)
 					//printf("%d %d, %3.2f %3.2f %3.2f %3.2f %4.3f %3.2f;  ", i0, j, fBest_Fractional_Synthesis_Rate_Time[i, j - 1], Fractional_Synthesis_Rate[i0, j], 
@@ -97,7 +102,7 @@ int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional
 						//Score_Fractional_Synthesis_Rate_Time[i, j]);
 					if (fSum >= Score_Fractional_Synthesis_Rate_Time[i, j])
 					{
-					
+
 						//when multiple paths are possible, choose the path with the least slope
 						// this should always find a solution, if it exists.
 						//if (fMinSlope > Math::Abs(Fractional_Synthesis_Rate[i0, j] - fBest_Fractional_Synthesis_Rate_Time[i, j - 1]))
@@ -142,20 +147,25 @@ int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional
 	printf("Best path\n");
 
 
+	String^ temp_path = "";
 	for (i = 0; i < Nrow; i++)
 	{
+		temp_path = "";
 		for (j = 0; j < Ncol; j++)
 		{
-			printf("Next[%d, %d] = %d %d  ", i, j, index_X[i, j], index_Y[i,j]);
+			printf("Next[%d, %d] = %d %d  ", i, j, index_X[i, j], index_Y[i, j]);
+			temp_path = temp_path + i.ToString() + "," + j.ToString() + " ";
 		}
 		puts("");
+		bestpaths->Add(temp_path->Trim());
 	}
 
 	ftemp = -1000000.;
 
 	for (i = 0; i < Nrow; i++)
 	{
-		printf("%10.5f\n", Score_Fractional_Synthesis_Rate_Time[i, Ncol - 1]);
+		printf("tt %10.5f\n", Score_Fractional_Synthesis_Rate_Time[i, Ncol - 1]);
+		bestpaths_score->Add(Score_Fractional_Synthesis_Rate_Time[i, Ncol - 1]);
 
 		if (Score_Fractional_Synthesis_Rate_Time[i, Ncol - 1] > ftemp)
 		{
@@ -165,7 +175,7 @@ int Label_Path::Labeling_Path_Fractional_Synthesis(array <float, 2> ^ Fractional
 		}
 	}
 
-	
+
 
 
 
@@ -180,26 +190,26 @@ float Label_Path::Diff_Fraction_Rates(float FRate1, float FRate2)
 {
 	if (FRate2 >= FRate1)
 	{
-		if(false)
-		if (FRate2 > 1.1)
-		{
-			return -1. * (FRate2 - 1.1) ;
-		}
-		else
-		{
-			return 1.0;
-		}
+		if (false)
+			if (FRate2 > 1.1)
+			{
+				return -1. * (FRate2 - 1.1);
+			}
+			else
+			{
+				return 1.0;
+			}
 		else
 		{
 			return 1.2 - (FRate2 - FRate1);
 		}
-		
+
 	}
 	else
 	{
 		if (FRate2 < FRate1)
 		{
-			return -1.0 *(FRate1 - FRate2);
+			return -1.0 * (FRate1 - FRate2);
 		}
 		//else
 			//return -1.0;
